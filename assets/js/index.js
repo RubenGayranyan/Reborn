40798d3bd16f409ee026680f9ace0ea34986200f
RubenGayranyan <102463098+RubenGayranyan@users.noreply.github.com>
2024-08-26 17:42:46 +0000

    Delete assets directory

--- assets/js/index.js
+++
@@ -1,8 +0,0 @@
-function toggleMobileNavigation() {
- 	let navBar = document.getElementById("mobileNav");
-	if(navBar.style.display === "none") {
-    		navBar.style.display = "block";
-    	} else {
-	    navBar.style.display = "none";
-	}
-};
