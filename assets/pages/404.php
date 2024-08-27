f6734ae963d539179b4fbe93fcc156da3aec1ff6
RubenGayranyan <102463098+RubenGayranyan@users.noreply.github.com>
2024-08-27 14:13:04 +0000

    Add files via upload

--- assets/pages/404.php
+++ assets/pages/404.php
@@ -1,4 +1,7 @@
 <title>Հմ․․․ Կարծես դուք սխալվել եք | Reborn Role Play</title>
-
-<div class="contentBlock"><center>Ցավոք, Ձեր տվյալ էջը գոյություն չունի, կամ դուք չունեք բավարար հասանելիություն։<br />
-    Խնդրում ենք վերադառնալ <a href="/"><strong>գլխավոր էջ</strong></a>։</center></div>
+<div class="contentBlock">
+    <div class="contentBlockWrapper">
+        <center>Ցավոք, Ձեր տվյալ էջը գոյություն չունի, կամ դուք չունեք բավարար հասանելիություն։<br />
+    Խնդրում ենք վերադառնալ <a href="/"><strong>գլխավոր էջ</strong></a>։</center>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -4,7 +4,6 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="assets/css/style.css?v26.08.2024">
-        <link rel="stylesheet" href="assets/css/media.css?v26.08.2024">
 	<script src="assets/js/index.js"></script>
 
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">        <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -13,30 +12,26 @@
     </head>
     <body class="pBody">
     	<?php
-    	
             global $page_id;
             $page_id = ( isset( $_REQUEST['page'] ) ) ? (int) $_REQUEST['page'] : 1;
-
         ?>
-    
         <div class="pWrapper">
             <?php include "assets/pages/modules/navigation.php"; ?>
             <?php include "assets/pages/modules/mobileNavigation.php"; ?>
             <div class="pContent">
-		    <?php 
-		       	$page = "assets/pages/page" . $page_id . ".php";
+    		    <?php 
+    		       	$page = "assets/pages/page" . $page_id . ".php";
 
-		        if ( file_exists( $page ) ) {
-		            include $page; 
-		        } else {
-		            
-		            include "assets/pages/404.php";
+    		        if ( file_exists( $page ) ) {
+    		            include $page; 
+    		        } else {
+    		            
+    		            include "assets/pages/404.php";
 
-		        }
-		                        
-		    ?>
-		</div>
-            
+    		        }
+    		                        
+    		    ?>
+            </div>
             <?php include 'assets/pages/modules/footer.php';?>
         </div>
     </body>
